cfc2bbf61e44dc50f3eff080db8bde2342abc6d7
amvreddy <amvreddy78@gmail.com>
2019-01-14 19:49:29 +0530

    added new files

---
+++ Demo.vbs
@@ -0,0 +1,11 @@
+Dim Sum,i
+
+sum=0
+
+for i=0 to 5
+
+sum=sum+i
+
+
+next
+
---
+++ dataTypes.java
@@ -0,0 +1,40 @@
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+	System.out.println("Data Types");
+	
+	byte a=1;
+	System.out.println("Byte:"+a);		
+
+	short b=2;	
+	System.out.println("Short:"+b);
+
+	int c=4;
+	System.out.println("Int:"+c);
+	
+	long d=8;
+	System.out.println("Long:"+d);
+	
+	char f='X';
+		System.out.println("Char:"+f);
+	
+	float g=4.5f;
+	System.out.println("Float:"+g);
+	
+	double h=8.6677888;
+	System.out.println("Double:"+h);
+	
+	boolean i;
+	
+	
+	if (i=false)
+		System.out.println("Boolean:"+i);
+		//System.out.println("hi");
+	else
+	 
+		System.out.println("Boolean:"+i);
+	
+			
+	}
+}
